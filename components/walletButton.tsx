7d8c05cf4fde589141501fa1e76aa7c4fe558f92
Octa-X <bigkala166@gmail.com>
2026-04-25 15:08:13 +0200

    Add WalletButton component for wallet connection

---
+++ components/walletButton.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useAccount, useConnect, useDisconnect } from "wagmi";
+
+export default function WalletButton() {
+  const { address, isConnected } = useAccount();
+  const { connect, connectors } = useConnect();
+  const { disconnect } = useDisconnect();
+
+  if (isConnected) {
+    return (
+      <div>
+        <p>Connected: {address}</p>
+        <button onClick={() => disconnect()}>Disconnect</button>
+      </div>
+    );
+  }
+
+  return (
+    <button onClick={() => connect({ connector: connectors[0] })}>
+      Connect Wallet
+    </button>
+  );
+}
